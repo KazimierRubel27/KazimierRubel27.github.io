a4bedaea97b63d482c9c3edb0da7ccbd7b251484
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-04-24 18:01:40 +0000

    5 tasks main.js

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+console.log("Task 1");
+for (let i = 0; i <= 10; i++){
+    console.log(i * i);
+}
+console.log("Task 2")
+for (let i = 5; i > 0; i--){
+    console.log(i);
+}
+console.log("Blastoff!");
+console.log("Task 3");
+for (let i = 2; i<= 50; i+=2){
+    console.log(i);
+}
+console.log("Task 4");
+let total = 0
+for (let i = 1; i<=100; i++){
+    total = total += i;
+}
+console.log(total);
+console.log("Task 5");
+let factorial = 1;
+for (let i = 1; i <= 10; i++) {
+    factorial *= i;
+    console.log(factorial);
+}
