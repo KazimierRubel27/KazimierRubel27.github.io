37d6b529a7c5b65a4e2b84b786993a9755165bd1
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-04-26 17:01:43 +0000

    task 6-10 main.js

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,33 @@ for (let i = 1; i <= 10; i++) {
     factorial *= i;
     console.log(factorial);
 }
+console.log("Task 6")
+let arr = [3, 1, 4, 1, 5];
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i]);
+}
+console.log("Task 7")
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr.at(i));
+}
+console.log("Task 8")
+let cubes = [];
+for (let i = 1; i <= 10; i++) {
+    cubes.push(i ** 3);
+}
+console.log(cubes)
+console.log("Task 9")
+let fibonnaci = []
+fibonnaci.push(1)
+fibonnaci.push(1)
+for (let i = 1; i <= 8; i++){
+    sum = fibonnaci[i] + fibonnaci[i-1]; 
+    fibonnaci.push(sum) 
+}
+console.log(fibonnaci)
+console.log("Task 10")
+let reversedArr = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr.push(arr[i]);
+}
+console.log(reversedArr);
