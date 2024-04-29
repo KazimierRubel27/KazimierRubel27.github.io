247fd2a6cdd92b498dff634190993d386bb00de1
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-04-29 16:10:46 +0000

    fixed task 7

--- scripts/main.js
+++ scripts/main.js
@@ -29,8 +29,10 @@ for (let i = 0; i <= arr.length - 1; i++) {
     console.log(arr[i]);
 }
 console.log("Task 7")
-for (let i = arr.length - 1; i >= 0; i--) {
-    console.log(arr.at(i));
+let negative = 0
+for (let i = 0; i < arr.length; i++) {
+    negative = (-i -1);
+    console.log(arr.at(negative));
 }
 console.log("Task 8")
 let cubes = [];
