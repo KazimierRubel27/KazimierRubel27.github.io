47a4c26a0c7f8bbe44c56bd7bb25ae780986aecb
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-05-29 20:12:48 +0000

    add css to about me

--- scripts/about.js
+++ scripts/about.js
@@ -6,6 +6,9 @@ let inputBox = document.getElementById("inputBox");
 document.getElementById("addButton").addEventListener("click", function () {
     let li = document.createElement('li');
     li.innerText = inputBox.value;
-    list.appendChild(li);
+    if (inputBox.value.toLowerCase()) === "yellow hat"{
+        list.appendChild(li + "link to the man in the yellow hat" + "yellow_hat.html");
+    } else {
+        list.appendChild(li);
+    }
 });
-
