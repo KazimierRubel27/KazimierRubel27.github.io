f677b4350c43bea2c01532d8449fcf8b3f9a5533
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-05-31 18:21:57 +0000

    change equals to contains for all yellow hat

--- scripts/about.js
+++ scripts/about.js
@@ -5,7 +5,7 @@ let inputBox = document.getElementById("inputBox");
 
 document.getElementById("addButton").addEventListener("click", function () {
     let li = document.createElement('li');
-    if (inputBox.value.toLowerCase() === "yellow hat"){
+    if (inputBox.value.toLowerCase().includes("yellow hat")){
         let anchorLink = document.createElement("a");
         anchorLink.href = "yellow_hat.html";
         anchorLink.innerText = inputBox.value;
