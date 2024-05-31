06eb56398d359f7288fd20fefc2ee623f26bca53
KazimierRubel27 <144712720+KazimierRubel27@users.noreply.github.com>
2024-05-31 16:19:46 +0000

    add spinning george

--- scripts/about.js
+++ scripts/about.js
@@ -7,7 +7,10 @@ document.getElementById("addButton").addEventListener("click", function () {
     let li = document.createElement('li');
     li.innerText = inputBox.value;
     if (inputBox.value.toLowerCase()) === "yellow hat"{
-        list.appendChild(li + "link to the man in the yellow hat" + "yellow_hat.html");
+        let anchorLink = document.createElement("a");
+        anchorLink.href = "yellow_hat.html";
+        li.appendChild(anchorLink);
+        list.appendChild(li);
     } else {
         list.appendChild(li);
     }
